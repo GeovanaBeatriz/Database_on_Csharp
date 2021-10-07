1d90f063d6bb39ba12b71a94a4bd232ff677d7c0
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2021-10-06 21:32:11 -0300

    Tela Splash e Inicialização do Formulário Vendas no Frm Principal

--- GeovanaBeatrizdeJesus_BD/Form1.cs
+++ GeovanaBeatrizdeJesus_BD/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace GeovanaBeatrizdeJesus_BD
 {
@@ -15,6 +16,14 @@ namespace GeovanaBeatrizdeJesus_BD
         public FrmPrincipal()
         {
             InitializeComponent();
+
+            frmSplashScreen frmSplashScreen = new frmSplashScreen();
+            //Exibindo o SplashScreen
+            frmSplashScreen.Show();
+            //Tempo que irá aparecer em milisegundos
+            Thread.Sleep(7500);
+            //Fechando o SplashScreen
+            frmSplashScreen.Close();
         }
 
         private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,6 +40,14 @@ namespace GeovanaBeatrizdeJesus_BD
             frmProd.MdiParent = this; //Esta linha define que o formulário cliente é 'filho' do formulário
             //principal e portanto, deverá abrir dentro dele
             frmProd.Show(); //exibe o formulário cliente
+        }
+
+        private void vendasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmVendas frmVendas = new frmVendas(); //Criando instância do formulário Clientes
+            frmVendas.MdiParent = this; //Esta linha define que o formulário cliente é 'filho' do formulário
+            //principal e portanto, deverá abrir dentro dele
+            frmVendas.Show(); //exibe o formulário cliente
         }
     }
 }
